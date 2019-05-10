ce68055fde6488876fa39503d44573f62628f149
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-05-09 23:39:59 -0400

    working aliases

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,6 +1,7 @@
 // enables intelligent code completion for Cypress commands
 // https://on.cypress.io/intelligent-code-completion
+import {greeting} from '@src/utils'
 it('shows greeting', function () {
   cy.visit('http://localhost:1234')
-  cy.contains('#app', 'Hello World').should('be.visible')
+  cy.contains('#app', greeting).should('be.visible')
 })
--- cypress/plugins/cy-ts-preprocessor.js
+++ cypress/plugins/cy-ts-preprocessor.js
@@ -1,8 +1,12 @@
 const wp = require('@cypress/webpack-preprocessor')
+const path = require('path')
 
 const webpackOptions = {
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
+    alias: {
+      '@src': path.resolve(__dirname, '../../src')
+    }
   },
   module: {
     rules: [
