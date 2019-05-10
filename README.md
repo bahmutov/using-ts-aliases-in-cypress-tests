93f3c68aadaab31a0b60920680fb24f3a67e9524
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-05-09 23:19:25 -0400

    working TS

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 - install dependencies with `npm ci`
 - start the local app with `npm start`
+- from another terminal start Cypress tests `npm run cy:open`
+
+You should see end-to-end tests from [cypress/integration/spec.ts](cypress/integration/spec.ts) running against the page.
--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,6 +1,5 @@
 // enables intelligent code completion for Cypress commands
 // https://on.cypress.io/intelligent-code-completion
-/// <reference types="Cypress" />
 it('shows greeting', function () {
   cy.visit('http://localhost:1234')
   cy.contains('#app', 'Hello World').should('be.visible')
